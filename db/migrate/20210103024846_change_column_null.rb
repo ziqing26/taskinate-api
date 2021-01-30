299584382d5cd3df48313467ca3f4cfe38b2e318
Zhang Ziqing <zhangziqing9926@gmail.com>
2021-01-30 09:58:32 +0800

    fixed minor issue

--- db/migrate/20210103024846_change_column_null.rb
+++ db/migrate/20210103024846_change_column_null.rb
@@ -1,4 +1,4 @@
-cclass ChangeColumnNull < ActiveRecord::Migration[6.0]
+class ChangeColumnNull < ActiveRecord::Migration[6.0]
   def change
     change_column :tasks, :done, :boolean, :default => false
   end
