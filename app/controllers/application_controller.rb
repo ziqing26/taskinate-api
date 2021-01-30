3be84109e5e678c2e461365b70d0ee2a9a0517b7
Zhang Ziqing <zhangziqing9926@gmail.com>
2021-01-30 11:18:21 +0800

    fix minor issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,16 @@ class ApplicationController < ActionController::API
     def login!
         session[:user_id] = @user.id
     end
-    def logged_in?
-        !!session[:user_id]
-    end
+
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
+
+    def logged_in?
+        # !!session[:user_id]
+        !!current_user
+    end
+    
     def authorized_user?
         @user == current_user
     end
